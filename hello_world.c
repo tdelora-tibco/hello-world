f014db5b14c8694f38eba6c818cc8c5fc4c09cdd
Thomas E De Lora <tdelors@tibco.com>
2020-07-30 11:14:31 -0700

    From home

--- hello_world.c
+++ hello_world.c
@@ -3,6 +3,6 @@ int main() {
    // printf() displays the string inside quotation
    // printf("Hello, World!");
    printf("Hello there Russ!");
-   printf("\nPull request CI run.");
+   printf("\nCreated on Mac and pushed");
    return 0;
 }

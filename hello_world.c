f255b44c77d55d18533efdd8b53d20c37d58a3d7
tdelora-tibco <61568044+tdelora-tibco@users.noreply.github.com>
2020-07-29 20:01:44 -0700

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -3,5 +3,6 @@ int main() {
    // printf() displays the string inside quotation
    // printf("Hello, World!");
    printf("Hello there Russ!");
+   printf("Pull request CI run.");
    return 0;
 }

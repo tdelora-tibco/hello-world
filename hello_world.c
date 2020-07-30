a0e3c7cd3655bf8b15a703a77810681d8c9d7736
tdelora-tibco <61568044+tdelora-tibco@users.noreply.github.com>
2020-07-29 17:43:59 -0700

    Update hello_world.c
    Updated output

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   // printf("Hello, World!");
+   printf("Hello there handsome!");
    return 0;
 }
